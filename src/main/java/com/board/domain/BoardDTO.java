e338f534c82b3202c7a7617fb34673684729bde2
hanhyunsoo <gustn4563@gmail.com>
2021-10-06 23:37:16 +0900

    Controller(create, update, delete)가 실행되면 alert창 뜨게하기, 백엔드 유효성검증(DTO와 컨트롤러에 javax.validation를 이용)

--- src/main/java/com/board/domain/BoardDTO.java
+++ src/main/java/com/board/domain/BoardDTO.java
@@ -3,6 +3,7 @@ package com.board.domain;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
+import javax.validation.constraints.*;
 
 @Getter
 @Setter
@@ -12,12 +13,15 @@ public class BoardDTO {
     private Long idx;
 
     /** 제목 */
+    @NotBlank(message = "title가 null 또는 빈 값")
     private String title;
 
     /** 내용 */
+    @NotBlank(message = "content가 null 또는 빈 값")
     private String content;
 
     /** 작성자 */
+    @NotBlank(message = "writer가 null 또는 빈 값")
     private String writer;
 
     /** 조회 수 */
