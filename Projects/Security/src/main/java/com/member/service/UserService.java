c88fa790b54cb33792f408da715b9a8787fd3153
hanhyunsoo <gustn4563@gmail.com>
2021-12-03 10:13:05 +0900

    Security config 설정, User entity, repository and service 추가

---
+++ Projects/Security/src/main/java/com/member/service/UserService.java
@@ -0,0 +1,22 @@
+package com.member.service;
+
+import com.member.domain.User;
+import com.member.domain.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(email));
+    }
+}
