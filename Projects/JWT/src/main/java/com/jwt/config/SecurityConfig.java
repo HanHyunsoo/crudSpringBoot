0f8445b646f3df60056f98c9461904192cb10a28
hanhyunsoo <gustn4563@gmail.com>
2021-12-04 21:12:18 +0900

    Security 설정, DB 설정

---
+++ Projects/JWT/src/main/java/com/jwt/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.jwt.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+    }
+}
