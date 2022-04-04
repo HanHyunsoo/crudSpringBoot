0ed2b70efc43c3e8b3405bb6819685d1d3499115
hanhyunsoo <gustn4563@gmail.com>
2022-04-04 19:31:30 +0900

    fix : Edit Repository

--- Projects/SNS/src/main/java/com/hyunsoo/sns/repository/UserRepository.java
+++ Projects/SNS/src/main/java/com/hyunsoo/sns/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.hyunsoo.sns.repository;
 
+import com.hyunsoo.sns.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserRepository, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
 }
