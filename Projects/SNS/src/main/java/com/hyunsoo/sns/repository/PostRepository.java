9f91c830d2654ffd26502b66f85984a6ed659af4
hanhyunsoo <gustn4563@gmail.com>
2022-04-07 18:40:13 +0900

    feat : Add ORM to Post Repository

--- Projects/SNS/src/main/java/com/hyunsoo/sns/repository/PostRepository.java
+++ Projects/SNS/src/main/java/com/hyunsoo/sns/repository/PostRepository.java
@@ -1,9 +1,14 @@
 package com.hyunsoo.sns.repository;
 
 import com.hyunsoo.sns.entity.Post;
+import com.hyunsoo.sns.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByUser(User user);
 }
