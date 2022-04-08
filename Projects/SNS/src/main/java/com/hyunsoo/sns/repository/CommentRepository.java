d5691c2e9c1921ce65624fdaa903a154a5a2e2c6
hanhyunsoo <gustn4563@gmail.com>
2022-04-08 13:34:47 +0900

    feat : Add ORM to Repository
    댓글, 대댓글 불러오는 orm 추가

--- Projects/SNS/src/main/java/com/hyunsoo/sns/repository/CommentRepository.java
+++ Projects/SNS/src/main/java/com/hyunsoo/sns/repository/CommentRepository.java
@@ -1,9 +1,27 @@
 package com.hyunsoo.sns.repository;
 
 import com.hyunsoo.sns.entity.Comment;
+import com.hyunsoo.sns.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    /**
+     * 해당 post의 모든 댓글 불러오기
+     * @param post : post entity
+     * @return List<Comment>
+     */
+    List<Comment> findAllByPostAndParentIsNull(Post post);
+
+    /**
+     * 해당 post와 부모 댓글을 이용해 모든 대댓글 불러오기
+     * @param post   : post entity
+     * @param parent : 부모 comment entity
+     * @return List<Comment>
+     */
+    List<Comment> findAllByPostAndParent(Post post, Comment parent);
 }
