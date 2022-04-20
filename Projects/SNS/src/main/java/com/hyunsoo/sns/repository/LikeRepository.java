6acd704045edb29656fbce9a8e8dc8551b1d9ea5
hanhyunsoo <gustn4563@gmail.com>
2022-04-20 10:22:17 +0900

    feat : Add ORM(findByUserAndPost)

--- Projects/SNS/src/main/java/com/hyunsoo/sns/repository/LikeRepository.java
+++ Projects/SNS/src/main/java/com/hyunsoo/sns/repository/LikeRepository.java
@@ -1,9 +1,15 @@
 package com.hyunsoo.sns.repository;
 
 import com.hyunsoo.sns.entity.Like;
+import com.hyunsoo.sns.entity.Post;
+import com.hyunsoo.sns.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    Optional<Like> findByUserAndPost(User user, Post post);
 }
