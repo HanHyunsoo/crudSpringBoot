894be00e70df470c0f97da3e227f3a89f8f37ef3
HanHyunSoo <gustn4563@gmail.com>
2022-05-10 15:26:57 +0900

    feat : Add DTO(message, room)

---
+++ Projects/Socket/src/main/java/com/socket/dto/ChatMessage.java
@@ -0,0 +1,18 @@
+package com.socket.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    public enum MessageType {
+        ENTER, TALK
+    }
+
+    private MessageType messageType;
+    private String roomId;
+    private String sender;
+    private String message;
+}
