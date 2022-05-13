1b7cd527f457d3113d8ff3740cfb515d7246fd04
HanHyunSoo <gustn4563@gmail.com>
2022-05-13 16:11:43 +0900

    fix : Set CORS

--- Projects/Socket/src/main/java/com/socket/config/WebSocketConfig.java
+++ Projects/Socket/src/main/java/com/socket/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.socket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
