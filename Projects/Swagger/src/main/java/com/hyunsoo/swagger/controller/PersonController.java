40db9c85f25d3a9c211234a4441717d8f94b41f3
HanHyunSoo <gustn4563@gmail.com>
2022-05-17 14:23:37 +0900

    feat : Add Controller

---
+++ Projects/Swagger/src/main/java/com/hyunsoo/swagger/controller/PersonController.java
@@ -0,0 +1,56 @@
+package com.hyunsoo.swagger.controller;
+
+import com.hyunsoo.swagger.dto.PersonRequest;
+import com.hyunsoo.swagger.dto.PersonResponse;
+import com.hyunsoo.swagger.service.PersonService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/people")
+public class PersonController {
+
+    private final PersonService personService;
+
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody final PersonRequest request) {
+        Long id = personService.save(request);
+
+        return ResponseEntity.created(URI.create("/api/people/" + id)).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PersonResponse>> findAll() {
+        List<PersonResponse> people = personService.findAll();
+
+        return ResponseEntity.ok(people);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonResponse> findById(@PathVariable final Long id) {
+        PersonResponse personResponse = personService.findById(id);
+
+        return ResponseEntity.ok(personResponse);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable final Long id,
+                                    @RequestBody final PersonRequest personRequest) {
+        personService.update(id, personRequest);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable final Long id) {
+        personService.deleteById(id);
+
+        return ResponseEntity.ok().build();
+    }
+}
