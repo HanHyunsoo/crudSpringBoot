e49b7c65edf8ed07e0014327ee36ad1280fa1dab
HanHyunSoo <gustn4563@gmail.com>
2022-05-17 14:12:54 +0900

    feat : Add DTO

---
+++ Projects/Swagger/src/main/java/com/hyunsoo/swagger/dto/PersonRequest.java
@@ -0,0 +1,12 @@
+package com.hyunsoo.swagger.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonRequest {
+
+    private String name;
+    private Short age;
+}
