29281938c32665ae65a865a464370f019d83abca
HanHyunSoo <gustn4563@gmail.com>
2022-05-17 13:34:58 +0900

    Start Swagger Project

---
+++ Projects/Swagger/src/main/java/com/hyunsoo/swagger/SwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.hyunsoo.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerApplication.class, args);
+    }
+
+}
